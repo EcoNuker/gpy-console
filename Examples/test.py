67d4ecc429146fe7ad83645fa99cd65515ea6cd9
YumYummity <103061664+YumYummity@users.noreply.github.com>
2024-07-28 23:29:21 -0400

    Create a guilded command for test

--- Examples/test.py
+++ Examples/test.py
@@ -1,24 +1,25 @@
 import guilded
+from guilded.ext import commands
 from gpyConsole import ConsoleBot
 from gpyConsole.context import Context
 
-client = ConsoleBot(
+bot = ConsoleBot(
     command_prefix="!",
     features=guilded.ClientFeatures(official_markdown=True),
 )
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
+    print(f"Logged in as {bot.user}")
 
 
-@client.event
+@bot.event
 async def on_console_message(message: str):
     print(f"Received command: {message}")
 
 
-@client.console_command()
+@bot.console_command()
 async def hey(
     ctx: Context,  # Modified Console Context with less features (.reply and .send are the same)
     channel: guilded.ChatChannel,
@@ -27,10 +28,15 @@ async def hey(
     # Missing converters: guilded.Role, guilded.Member, guilded.ChatMessage, guilded.Attachment
     await ctx.reply(f"Sending message to {user.name} (id: {user.id})")
     await channel.send(
-        f"Hello from Console! I'm {client.user.name}, and you are {user.mention}"
+        f"Hello from Console! I'm {bot.user.name}, and you are {user.mention}"
     )
 
+@bot.command()
+async def hi(
+    ctx: commands.Context
+):
+    await ctx.send(f"Hello {ctx.author.mention}!")
 
-client.run(
+bot.run(
     "gapi_g3ApoXCAoHXpEda7YvyU2KPHLepbBxHHIv0sdA7nnkJ5Gt353rFuDlWq8RdlYmer9C58jaTCXKjPjtcCcBLwgA=="
 )
